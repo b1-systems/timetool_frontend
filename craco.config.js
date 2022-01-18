d22feeb0c8705c80288ad595d183fa673d2ae823
Benedikt Jazdzewski <jazdzewski@b1-systems.de>
2022-01-18 11:51:46 +0100

    updated craco config

--- craco.config.js
+++ craco.config.js
@@ -5,26 +5,29 @@ module.exports = {
         overrideWebpackConfig: ({ webpackConfig }) => {
           const newWebpackConfig = Object.assign({}, webpackConfig);
 
+          // Enable the following line to see all config options
+          // console.log(require("util").inspect(newWebpackConfig, { depth: null }));
+
           newWebpackConfig.output.filename = "js/[name].js";
           newWebpackConfig.output.chunkFilename = "js/[name].chunk.js";
+          newWebpackConfig.output.assetModuleFilename = "themes/default/[name][ext]";
           newWebpackConfig.optimization.runtimeChunk = false;
           newWebpackConfig.optimization.splitChunks = {
             cacheGroups: {
               default: false,
             },
           };
+
           // modify MiniCssExtractPlugin
-          newWebpackConfig.plugins[4].options.filename = "themes/default/[name].css";
-          newWebpackConfig.plugins[4].options.chunkFilename =
-            "themes/default/[name].chunk.css";
-
-          // modify media files
-          const moduleRules = newWebpackConfig.module.rules[1].oneOf;
-          newWebpackConfig.module.rules[1].oneOf[moduleRules.length - 1].options.name =
-            "themes/default/[name].[ext]";
-
-          // Enable the following line to see all config options
-          // console.log(require("util").inspect(newWebpackConfig, { depth: null }));
+          // the following approach is necessary since the order inside the `plugins`
+          // array is not stable
+          const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+          newWebpackConfig.plugins.forEach((plugin) => {
+            if (plugin instanceof MiniCssExtractPlugin) {
+              plugin.options.filename = "themes/default/[name].css";
+              plugin.options.chunkFilename = "themes/default/[name].chunk.css";
+            }
+          });
 
           return newWebpackConfig;
         },
